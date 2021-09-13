34519593f400ce475e3d4c1347f74292b1803575
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-09-14 00:47:57 +0100

    footer component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Home from './views/Home';
 
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
