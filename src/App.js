fbc955f37775f652f89bae277da812100b456db0
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-10-10 20:54:36 +0100

    data table displayed on cryptocurrency page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react'
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Home from './views/Home';
+import Cryptocurrencies from './views/Cryptocurrencies';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Home />
-      {/* <Footer /> */}
-      
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/cryptocurrencies" component={Cryptocurrencies} />
+      </Switch>
+      {/* <Home />
+      <Cryptocurrencies /> */}
     </Router>
   );
 }
