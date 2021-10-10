a874e75ed8de4ba9643c81a08c23217d8fb5c660
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-10-10 17:35:47 +0100

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './views/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
---
+++ src/components/Navbar.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import Logo from '../assets/images/blockchain.png'
+import { AiOutlineClose } from "react-icons/ai";
+import { BiHome } from "react-icons/bi";
+import { HiOutlineMenuAlt1 } from "react-icons/hi";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { FaExchangeAlt } from "react-icons/fa";
+import { GiNewspaper } from "react-icons/gi";
+
+const menuItems = [
+  {
+    title: "Home",
+    url: "/",
+    icon: <BiHome />
+  },
+  {
+    title: "Cryptocurrencies",
+    url: "/cryptocurrencies",
+    icon: <BsCurrencyDollar />
+  },
+  {
+    title: "Exchanges",
+    url: "/exchanges",
+    icon: <FaExchangeAlt />
+  },
+  {
+    title: "News",
+    url: "/news",
+    icon: <GiNewspaper />
+  },
+];
+
+
+const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const handleClick = () => {
+      setActive(!active);
+  };
+    
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar-logo">
+        <img src={Logo} alt="logo" width="40px" /> Crypthub
+      </Link>
+      <div className="menu-icon" onClick={handleClick}>
+        {active
+          ? <AiOutlineClose color="white" className="close" size="28px" />
+          : <HiOutlineMenuAlt1 color="white" className="open" size="32px" />}
+      </div>
+      <ul className={active ? "nav-menu active" : "nav-menu"}>
+        {menuItems.map((item, index) => {
+          return (
+            <Link to={item.url}  className="nav-links">
+              <li key={index}>
+                <span className="nav-icon">
+                  {item.icon}
+                </span>
+                {item.title}
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
