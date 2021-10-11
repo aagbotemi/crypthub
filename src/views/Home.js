7395d1db17a93a7ce1cafc75ace101bac772e7d3
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-10-11 22:44:04 +0100

    data on hero page updated

--- src/views/Home.js
+++ src/views/Home.js
@@ -12,15 +12,14 @@ const Home = () => {
     // const percentage = (total24hVolume / totalMarketCap) * 100;
 
     /// console.log(percentage);
-    console.log(data);
+    console.log(data?.data);
 
     return (
         <section className="home">
             <article className="hero">
                 <div className="leftHandSide">
                     <h1>Join the best cryptocurrency platform</h1>
-                    <p>The global crypto market cap is <span className="bold">{ currency }{numFormatter(globalStats?.totalMarketCap)}</span>, a <span className="green bold">2.234%</span> increase over the last day.</p>
-                    <p>The total crypto market volume over the last 24 hours is <span className="bold">{ currency }{numFormatter(globalStats?.total24hVolume)}</span>, which makes a <span className="green bold">28.15%</span> increase. The total volume in DeFi is currently <span className="bold">$18.42B</span>, <span className="bold">13.00%</span> of the total crypto market 24-hour volume. The volume of all stable coins is now <span className="bold">$109.59B</span>, which is <span className="bold">77.36%</span> of the total crypto market 24-hour volume.</p>
+                    <p>The global crypto market cap is <span className="bold">{ currency }{numFormatter(globalStats?.totalMarketCap)}</span>. The total crypto market volume over the last 24 hours is <span className="bold">{ currency }{numFormatter(globalStats?.total24hVolume)}</span>.</p>
                 </div>
                 <div className="rightHandSide">
                     <img src={HeroImg} alt="hero" />
