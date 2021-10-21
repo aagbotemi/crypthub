4b7cb6a50b134fa2925ae1a3e289851f711ca288
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-10-22 00:54:26 +0100

    more information on the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import Logo from '../assets/images/logo.png'
+import { AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai'
+import { FaFacebook, FaLinkedin } from 'react-icons/fa'
 
 const Footer = () => {
     return (
-        <footer className="text-center">
+        <footer className="">
+            <div>
+                <div>
+                    <img src={Logo} width="20px" alt="crypthub logo" /> <span className="">Crypthub</span>
+                </div>
+                <p>Your number one platform to get real-time update about cryptocurrencies</p>
+            </div>
+            <div className="social-icons">
+                <h2>
+                    Socials
+                </h2>
+                <span><AiOutlineInstagram size="25px" /></span>
+                <span><AiOutlineTwitter size="25px" /></span>
+                <span><FaLinkedin size="25px" /></span>
+                <span><FaFacebook size="25px" /></span>
+            </div>
             <div>
             Designed & Developed by <a href="https://github.com/aagbotemi" target="_blank" rel="noopener noreferrer">Abiodun Awoyemi</a>
             </div>
-            Copyright &copy; {new Date().getFullYear()}
+            Copyright &copy; {new Date().getFullYear()} Crypthub
         </footer>
     )
 }
