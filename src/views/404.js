2ebcf48ade4d51f6d5be8c98e5c87cfe557d00c8
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-10-22 02:00:45 +0100

    seo added to 404 page

--- src/views/404.js
+++ src/views/404.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Helmet } from 'react-helmet';
 
 const NotFound = () => {
     return (
         <div className="error-page-container">
+            <Helmet>
+                <title>404 | Crypthub</title>
+                <meta name="description" content="Error page. Page not found" />
+            </Helmet>
             <div className="error-page">
                 <h1 className="">Page Not Found</h1>
                 <Link to='/' className="">Back to home</Link>
