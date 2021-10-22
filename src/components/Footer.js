d4bbdcf0fd1fa2e9f7837c48c794de7535de2f7c
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-10-22 01:39:48 +0100

    css style added to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,26 +5,35 @@ import { FaFacebook, FaLinkedin } from 'react-icons/fa'
 
 const Footer = () => {
     return (
-        <footer className="">
-            <div>
-                <div>
-                    <img src={Logo} width="20px" alt="crypthub logo" /> <span className="">Crypthub</span>
+        <footer>
+            <div className="footer-upper">
+                <div className="company-details">
+                    <div className="company-details-brand">
+                        <img src={Logo} width="18px" alt="crypthub logo" />
+                        <span className="">Crypthub</span>
+                    </div>
+                    <p>Your number one platform to get real-time update about cryptocurrencies</p>
                 </div>
-                <p>Your number one platform to get real-time update about cryptocurrencies</p>
+                <div className="social-icons">
+                    <h2>
+                        Socials
+                    </h2>
+                    <span><AiOutlineInstagram size="25px" /></span>
+                    <span><AiOutlineTwitter size="25px" /></span>
+                    <span><FaLinkedin size="25px" /></span>
+                    <span><FaFacebook size="25px" /></span>
+                </div>
             </div>
-            <div className="social-icons">
-                <h2>
-                    Socials
-                </h2>
-                <span><AiOutlineInstagram size="25px" /></span>
-                <span><AiOutlineTwitter size="25px" /></span>
-                <span><FaLinkedin size="25px" /></span>
-                <span><FaFacebook size="25px" /></span>
+            
+            <div className="footer-base">
+                
+                <div className="developer-details">
+                    Designed &amp; Developed by <a href="https://github.com/aagbotemi" target="_blank" rel="noopener noreferrer">Abiodun Awoyemi</a>
+                </div>
+                <div className="copyright">
+                    Copyright &copy; {new Date().getFullYear()} Crypthub
+                </div>
             </div>
-            <div>
-            Designed & Developed by <a href="https://github.com/aagbotemi" target="_blank" rel="noopener noreferrer">Abiodun Awoyemi</a>
-            </div>
-            Copyright &copy; {new Date().getFullYear()} Crypthub
         </footer>
     )
 }
